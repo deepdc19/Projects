a08f9e0c532278fdcc2103830fb13765450a3ba9
Deep <deepdeep.ac@gmail.com>
2018-09-01 16:57:01 -0400

    Methods added to stack

--- stack.py
+++ stack.py
@@ -1,1 +1,25 @@
+class Node:
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next
+
+class Stack:
+    def __init__(self, top = None):
+        self.top = top
+    
+    def push(self, data):
+        self.top = Node(data, self.top)
+    
+    def pull(self):
+        if self.top is None:
+            return None
+        temp = self.top.data
+        self.top = self.top.next
+        return temp
+    
+    def peek(self):
+        return None if self.top is None else self.top.data
+    
+    def IsEmpty(self):
+        return self.peek() is None
 
